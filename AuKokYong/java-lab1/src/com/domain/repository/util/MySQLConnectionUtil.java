676fff782b7ad1bc9912b4457ca923c9016d381f
aukokyong <75824065+aukokyong@users.noreply.github.com>
2021-05-04 09:48:16 +0800

    [lab-1] add connection to msql

--- AuKokYong/java-lab1/src/com/domain/repository/util/MySQLConnectionUtil.java
+++ AuKokYong/java-lab1/src/com/domain/repository/util/MySQLConnectionUtil.java
@@ -1,4 +1,24 @@
 package com.domain.repository.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MySQLConnectionUtil {
+    
+    public static void main(String[] args){
+        getConnection();
+    }
+    
+    public static Connection getConnection(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/customer","root","");
+            System.out.println("Connected to db");
+            return connection;
+        } catch(ClassNotFoundException | SQLException exception){
+            System.out.println(exception);
+            return null;
+        }
+    }
 }
