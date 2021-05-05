4b8c185d036b33e2594795202799ce9c6ff13d91
KohHongYe <Kohhongye@gmail.com>
2021-05-05 09:56:47 +0800

    Add DSSProjREST-V2

---
+++ DSSProjREST-V2/src/main/java/com/dss/DssProjRestV2Application.java
@@ -0,0 +1,14 @@
+package com.dss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssProjRestV2Application {
+
+	public static void main(String[] args) {
+		System.out.println("DSS Project Rest V2 Application");
+		SpringApplication.run(DssProjRestV2Application.class, args);
+	}
+
+}
